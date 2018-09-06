9c8c5aca8a8f826e6bbbfa2f13b260d14e521116
Uriel Salischiker <uriel@urielsalis.me>
2018-09-06 12:38:18 -0300

    Add new job

--- src/main/java/me/urielsalis/projectpipeline/Main.kt
+++ src/main/java/me/urielsalis/projectpipeline/Main.kt
@@ -3,9 +3,7 @@ package me.urielsalis.projectpipeline
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
-import java.io.File
-import java.io.IOException
-import java.io.OutputStreamWriter
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.*
@@ -78,61 +76,63 @@ class New: CliktCommand(help="Create new pipeline from template") {
         "git add .pipeline".runCommand()
 
         echo("  --Creating job in jenkins")
-        with(URL(jenkinsHost+"/createItem?name=$name").openConnection() as HttpURLConnection) {
+        with(URL("$jenkinsHost/createItem?name=$name").openConnection() as HttpURLConnection) {
             requestMethod = "POST"
+            doOutput = true
+            val auth = Base64.getEncoder().encode(jenkinsUserPass.toByteArray(Charsets.UTF_8)).toString(Charsets.UTF_8)
 
             addRequestProperty("Content-Type", "application/xml")
-            addRequestProperty("Authorization", Base64.getEncoder().encode(jenkinsUserPass.toByteArray(Charsets.UTF_8)).toString(Charsets.UTF_8))
+            addRequestProperty("Authorization", "Basic $auth")
             val wr = OutputStreamWriter(outputStream)
-            wr.write("""
-                <?xml version='1.<flow-definition plugin="workflow-job@2.24">
-                        <actions>
-                            <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@1.3.2"/>
-                            <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@1.3.2">
-                                <jobProperties/>
-                                <triggers/>
-                                <parameters/>
-                                <options/>
-                            </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
-                        </actions>
-                        <description></description>
-                        <keepDependencies>false</keepDependencies>
-                        <properties>
-                            <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-                                <triggers>
-                                    <com.cloudbees.jenkins.GitHubPushTrigger plugin="github@1.29.2">
-                                        <spec></spec>
-                                    </com.cloudbees.jenkins.GitHubPushTrigger>
-                                </triggers>
-                            </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-                            <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.29.2">
-                                <projectUrl>$projectUrl</projectUrl>
-                                <displayName>$name</displayName>
-                            </com.coravy.hudson.plugins.github.GithubProjectProperty>
-                        </properties>
-                        <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition" plugin="workflow-cps@2.54">
-                            <scm class="hudson.plugins.git.GitSCM" plugin="git@3.9.1">
-                                <configVersion>2</configVersion>
-                                <userRemoteConfigs>
-                                    <hudson.plugins.git.UserRemoteConfig>
-                                        <url>$projectUrl</url>
-                                    </hudson.plugins.git.UserRemoteConfig>
-                                </userRemoteConfigs>
-                                <branches>
-                                    <hudson.plugins.git.BranchSpec>
-                                        <name>*/master</name>
-                                    </hudson.plugins.git.BranchSpec>
-                                </branches>
-                                <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
-                                <submoduleCfg class="list"/>
-                                <extensions/>
-                            </scm>
-                            <scriptPath>.pipeline/Jenkinsfile</scriptPath>
-                            <lightweight>false</lightweight>
-                        </definition>
-                        <triggers/>
-                        <disabled>false</disabled>
-                    </flow-definition>1' encoding='UTF-8'?>
+            wr.write("""<?xml version='1.1' encoding='UTF-8'?>
+                <flow-definition plugin="workflow-job@2.24">
+                    <actions>
+                        <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@1.3.2"/>
+                        <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@1.3.2">
+                            <jobProperties/>
+                            <triggers/>
+                            <parameters/>
+                            <options/>
+                        </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
+                    </actions>
+                    <description>Auto generated from template $template</description>
+                    <keepDependencies>false</keepDependencies>
+                    <properties>
+                        <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+                            <triggers>
+                                <com.cloudbees.jenkins.GitHubPushTrigger plugin="github@1.29.2">
+                                    <spec></spec>
+                                </com.cloudbees.jenkins.GitHubPushTrigger>
+                            </triggers>
+                        </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+                        <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.29.2">
+                            <projectUrl>$projectUrl</projectUrl>
+                            <displayName>$name</displayName>
+                        </com.coravy.hudson.plugins.github.GithubProjectProperty>
+                    </properties>
+                    <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition" plugin="workflow-cps@2.54">
+                        <scm class="hudson.plugins.git.GitSCM" plugin="git@3.9.1">
+                            <configVersion>2</configVersion>
+                            <userRemoteConfigs>
+                                <hudson.plugins.git.UserRemoteConfig>
+                                    <url>$projectUrl.git</url>
+                                </hudson.plugins.git.UserRemoteConfig>
+                            </userRemoteConfigs>
+                            <branches>
+                                <hudson.plugins.git.BranchSpec>
+                                    <name>*/master</name>
+                                </hudson.plugins.git.BranchSpec>
+                            </branches>
+                            <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+                            <submoduleCfg class="list"/>
+                            <extensions/>
+                        </scm>
+                        <scriptPath>.pipeline/Jenkinsfile</scriptPath>
+                        <lightweight>false</lightweight>
+                    </definition>
+                    <triggers/>
+                    <disabled>false</disabled>
+                </flow-definition>
             """.trimIndent())
             wr.flush()
 
